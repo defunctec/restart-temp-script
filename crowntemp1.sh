635a11a71e61182cf0a727131b67c2234f6d238c
defunctec <defuncteconomics@gmail.com>
2019-04-29 21:37:24 +0100

    Real test 2
    Test 2

--- crowntemp1.sh
+++ crowntemp1.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 download_script() {
-    wget -c https://www.dropbox.com/s/lfb5nr98umvgoly/crowntemp2.sh?dl=0 -O crowntemp2.sh
+    wget -c "https://www.dropbox.com/s/lfb5nr98umvgoly/crowntemp2.sh?dl=0" -O crowntemp2.sh
 }
 
 add_permission() {
     sudo chmod 755 crowntemp2.sh
 }
 
+dos_to_unix() {
+    sudo apt install dos2unix
+    sudo dos2unix crowntemp2.sh
+}
+
 add_cron_job() {
-    cron_line2="9 17 * * * ./crowntemp2.sh"
+    cron_line2="* 11 * * * ./crowntemp2.sh"
     if [ `crontab -l 2>/dev/null | grep "$cron_line2" | wc -l` -eq 0 ]; then
         (crontab -l 2>/dev/null; echo "$cron_line2") | crontab -
     fi
@@ -22,11 +27,13 @@ main() {
     download_script
     # Permissions for script
     add_permission
+    # Change from dos to unix
+    dos_to_unix
     # Ensure there is a cron job to restart crownd on reboot
     add_cron_job
     # Allow wallet to shutdown
-    echo "Please wait 1 minuets while the Crown client restarts..."
-    sleep 60
+    echo "Please wait 5 minuets while the Crown client restarts..."
+    sleep 300
     # Start Crownd to begin sync
     /usr/local/bin/crownd
 }
