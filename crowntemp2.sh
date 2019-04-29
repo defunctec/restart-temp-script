0effb70795c58776b4d084fa0997f4f74a844082
defunctec <defuncteconomics@gmail.com>
2019-04-29 15:33:52 +0100

    Testing 2
    2

---
+++ crowntemp2.sh
@@ -0,0 +1,9 @@
+##!/bin/bash
+
+# 1 hours from 9am to 11pm, converted to minutes
+maxdelay=$((1*60))
+for ((i=1; i<=1; i++)); do
+	# pick an random delay
+    delay=$(($RANDOM%maxdelay)) 
+    (sleep $((delay*60)); /usr/local/bin/crown-cli restart) & # background a subshell to wait, then run the php script
+done
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Temporary script to restart the Crown client randomly.
+
+Install script
+
+```
+sudo wget "https://raw.githubusercontent.com/defunctec/restart-temp-script/master/crowntemp1.sh" | bash && sudo chmod +x crowntemp1.sh && sudo ./crowntemp1.sh
+```
